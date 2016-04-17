e5737ee056c3683d50c19160469a29d5f9236268
choijaewoong <jwchoi0129@gmail.com>
2016-04-17 13:37:57 +0000

    add email sending function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,4 @@
+require 'mailgun'
 class HomeController < ApplicationController
   def index
   end
@@ -8,5 +9,18 @@ class HomeController < ApplicationController
     @title = params[:title]
     @content = params[:content]
     
+    mg_client = Mailgun::Client.new("key-7c01232578d55b7cc10a70bf6b9cd54e")
+
+    message_params =  {
+                       from: 'jwchoi0129@gmail.com',
+                       to:   @email,
+                       subject: @title,
+                       text:    @content
+                      }
+    
+    result = mg_client.send_message('sandbox7ed8e5b58f5a401093943a3e8bb4a004.mailgun.org', message_params).to_h!
+    
+    message_id = result['id']
+    message = result['message']    
   end
 end
